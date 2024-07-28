c2939863a923e88eac73eaabec0e1cf9a7dc848a
Ulrich Kaiser <ulrich.kaiser@hmtm.de>
2024-07-28 14:18:02 +0200

    Show models in Display

--- src/music-puzzle-display.js
+++ src/music-puzzle-display.js
@@ -1,45 +1,35 @@
-import { Input, Button } from 'antd';
+import { Input } from 'antd';
+import AbcSnippet from './abc-snippet.js';
 import { useTranslation } from 'react-i18next';
-import React, { useState } from 'react';
-import Markdown from '@educandu/educandu/components/markdown.js';
+import React, { useEffect, useState } from 'react';
+import ModelComposition from './model-composition.js';
+import ModelProvider from './models/model-provider.js';
 import { sectionDisplayProps } from '@educandu/educandu/ui/default-prop-types.js';
-
-const { TextArea } = Input;
 
 export default function MusicPuzzleDisplay({ content }) {
 
   const { t } = useTranslation('musikisum/educandu-plugin-music-puzzle');
+  const { modelTemplates } = content;
 
-  const [abcResult, setAbcResult] = useState('');
+  const [abcResult, setAbcResult] = useState(''); 
 
-  const handleCurrentValueChange = event => {
-    // setAbcInput(event.target.value);
-  };
-  
-  const onButtonClick = () => {
-    // const playableABC = ModelComposition.abcOutput('C', 'C', '1/2=80', '1/2', []);
-    // setAbcResult(playableABC);
-  };
-
-  // useEffect(() => {}, []);
+  useEffect(() => {
+    if(modelTemplates.length > 0) {
+      const modelVoicesOfModels = modelTemplates.reduce((akku, modelTemplate)  => {
+        const model = ModelProvider.getModel(modelTemplate.name);
+        const modelVoices = model.getVoices(modelTemplate);
+        akku.push(modelVoices);
+        return akku;
+      }, []);
+        const playableABC = ModelComposition.abcOutput('C', 'C', '1/2=80', '1/2', modelVoicesOfModels, 6);
+        setAbcResult(playableABC);
+    }    
+  });
 
   return (
     <div className='EP_Educandu_Example_Display'>
       <div className={`u-horizontally-centered u-width-${content.width}`}>
-        <Markdown renderAnchors>{content.text}</Markdown>
-        {/* <Form layout='vertical'>
-          <Form.Item label={t('label')}>
-            <TextArea value={abcInput} maxLength={100} onChange={handleCurrentValueChange} />
-          </Form.Item>
-        </Form> */}
-        {/* <AbcSnippet playableABC={abcResult} /> */}
-        <Button onClick={onButtonClick}>Drückdrauf Junge ...</Button>
-        {/* {abcInput ? (
-          <div>
-            <AbcNotation abcCode={abcInput} onRender={setLastRenderResult} />
-            <AbcPlayer renderResult={lastRenderResult} />
-          </div>
-        ) : null} */}
+        { abcResult && <AbcSnippet playableABC={abcResult} /> }
       </div>
     </div>
   );
--- src/music-puzzle-editor.js
+++ src/music-puzzle-editor.js
@@ -20,35 +20,6 @@ export default function MusicPuzzleEditor({ content, onContentChanged }) {
     onContentChanged({ ...content, ...newContentValues });
   };
 
-  // const changeModelTemplateKey = (e, index) => {
-  //   const newModelTemplates = cloneDeep(modelTemplates);
-  //   const modelTemplateToUpdate = newModelTemplates[index];
-  //   modelTemplateToUpdate.key = e;
-  //   newModelTemplates[index] = modelTemplateToUpdate;
-  //   updateContent({ modelTemplates: newModelTemplates });
-  // };
-
-  // const onRadioChange = (e, index) => { 
-  //   const newModelTemplates = cloneDeep(modelTemplates);
-  //   const modelTemplateToUpdate = newModelTemplates[index];
-  //   modelTemplateToUpdate.radioValue = e.target.value;
-  //   newModelTemplates[index] = modelTemplateToUpdate;
-  //   updateContent({ modelTemplates: newModelTemplates }); 
-  // };
-
-  // const onArrowButtonClick = (direction, index) => {
-  //   const newModelTemplates = cloneDeep(modelTemplates);
-  //   const modelTemplateToUpdate = newModelTemplates[index];
-  //   const voice = modelTemplateToUpdate.radioValue;
-  //   if(direction === 'up') {
-  //     modelTemplateToUpdate.transposeValues[voice] += 1;
-  //   } else {
-  //     modelTemplateToUpdate.transposeValues[voice] -= 1;
-  //   }
-  //   newModelTemplates[index] = modelTemplateToUpdate;
-  //   updateContent({ modelTemplates: newModelTemplates });
-  // };
-
   const [selectedModel, setSelectedModel] = useState('cadence');
 
   const handleAddModelButtonClick = () => {
@@ -61,61 +32,6 @@ export default function MusicPuzzleEditor({ content, onContentChanged }) {
     newModelTemplates.push(modelTemplate);
     updateContent({ modelTemplates: newModelTemplates });
   };
-
-  // const renderModel = () => (
-  //   <React.Fragment>
-  //     { modelTemplates.map((modelTemplate, index) => (
-  //       <div className='container' key={index}>
-  //         <div className="left">
-  //           <div className='innerContainer'>
-  //             <div className='item-1'>
-  //               <div className='label'>Tonart</div>
-  //               <Select 
-  //                 defaultValue={modelTemplate.key} 
-  //                 options={ModelProvider.getModel(modelTemplate.name).getModelKeys().map(key => ({ value: key, label: key }))}
-  //                 onChange={e => changeModelTemplateKey(e, index)}
-  //                 />
-  //               <div>
-  //                 <AddProperties index={index} modelTemplates={modelTemplates} cloneDeep={cloneDeep} updateContent={updateContent} />
-  //               </div>                 
-  //             </div>
-  //             <div className='item-2'>
-  //               <div className='label'>Transposition (8)</div>
-  //               <div className='buttons'>
-  //                 <Button className='button' onClick={() => onArrowButtonClick('up', index)}><ArrowUpOutlined /></Button>
-  //                 <Button className='button' onClick={() => onArrowButtonClick('down', index)}><ArrowDownOutlined /></Button>
-  //               </div>
-  //               <Radio.Group onChange={e => onRadioChange(e, index)} value={modelTemplate.radioValue}>
-  //                 <Space direction="vertical">
-  //                   <Radio value={0}>{t('os')}</Radio>
-  //                   <Radio value={1}>{t('ms')}</Radio>
-  //                   <Radio value={2}>{t('us')}</Radio>
-  //                 </Space>
-  //               </Radio.Group>
-  //             </div>
-  //             <div className='item-3'>
-  //               <div className='label'>Stimmtausch</div>
-  //               <VoiceSwitch style={{ margin: '16px 0' }} modelIndex={index} modelTemplates={modelTemplates} updateContent={updateContent} />
-  //             </div>
-  //           </div>
-  //         </div>
-  //         <div className="right">
-  //           <div>
-  //             <AbcSnippet 
-  //               playableABC={
-  //                 ModelComposition.abcOutput('C', 'C', 120, '1/2', [
-  //                   ModelProvider
-  //                     .getModel(modelTemplate.name)
-  //                     .getVoices(modelTemplate)
-  //                 ])
-  //               }
-  //               />
-  //           </div>
-  //         </div>
-  //       </div>)
-  //     )}
-  //   </React.Fragment>
-  // );
 
   const handleItemMove = (fromIndex, toIndex) => {
     const newModelTemplates = moveItem(modelTemplates, fromIndex, toIndex);

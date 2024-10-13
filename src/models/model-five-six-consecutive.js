3e279ac3a10b8e1d71df2bdcfd2c547e173b0df9
musikisum <ulrich.kaiser@hmtm.de>
2024-10-13 23:32:01 +0200

    Convert 56-model to new data format

--- src/models/model-five-six-consecutive.js
+++ src/models/model-five-six-consecutive.js
@@ -1,25 +1,8 @@
 import ModelUtilities from '../model-utilities.js';
 import ModelTemplates from '../model-templates.js';
-import cloneDeep from '@educandu/educandu/utils/clone-deep.js';
-
-const _keyObj = {
-  'E': { modelKey: 'E', t: 2, accidentals: [[0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1], [1, 1, 1, 0, 1, 0, 0, 1, 1, 1, 1, 0], [1, 0, 1, 1, '^^', 1, 1, 0, 1, 0, 1, 1]] },
-  'A': { modelKey: 'A', t: -2, accidentals: [[0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1], [0, 1, 1, 0, 1, 0, 0, 1, 1, 1, 1, 0], [1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1]] },
-  'D': { modelKey: 'D', t: 1, accidentals: [[0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1], [0, 1, 1, 0, 1, 0, 0, 0, 0, 1, 1, 0], [1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0]] },
-  'G': { modelKey: 'G', t: 4, accidentals: [[0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0], [1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0]] },
-  'C': { modelKey: 'C', t: 0, accidentals: [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0], [0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0]] },  
-  'F': { modelKey: 'F', t: 3, accidentals: [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 1, 0, 1, 0, 0, -1, '=', 0, 1, 0]] },
-  'Bb': { modelKey: 'Bb', t: -1, accidentals: [[0, 0, 0, 0, 0, 0, -1, -1, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1], [0, -1, '=', 0, 1, 0, 0, -1, '=', 0, 1, 0]] },
-  'Eb': { modelKey: 'Eb', t: 2, accidentals: [[-1, -1, 0, 0, 0, 0, -1, -1, 0, 0, 0, 0], [0, 0, 0, 0, 0, -1, 0, 0, 0, 0, 0, -1], [0, -1, '=', 0, 1, 0, 0, -1, '=', -1, '=', 0]] },
-  'Ab': { modelKey: 'Eb', t: -2, accidentals: [[-1, -1, 0, 0, 0, 0, -1, -1, -1, -1, 0, 0], [-1, 0, 0, -1, '=', -1, 0, 0, 0, 0, 0, -1], [0, -1, '=', -1, '=', 0, 0, -1, '=', -1, '=', 0]] },
-};
-
-function _getKeyObject(change) {  
-  return _keyObj[change ?? 'C'];
-}
 
 function getModelKeys() {
-  return Object.keys(_keyObj);
+  return ['E', 'A', 'D', 'G', 'C', 'F', 'Bb', 'Eb', 'Ab'];
 }
 
 const getOptions = change => {
@@ -31,31 +14,25 @@ const getOptions = change => {
 };
 
 const getVoices = modelOptions => {
-  const voicesLength = 12;
-  const measure = [' | ', ' ', ' | ', ' ', ' | ', ' ', ' | ', ' ', ' | ', ' ', ' | ', ' '];
-  const voices = [[4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9], [1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7], [-1, 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5]];
   const options = modelOptions ?? getOptions();
-  let keyObject = _getKeyObject(options.modelKey);
+  let voices = [
+    ['=G42', '=G42', '=A42', '=A42', '=B42', '=B42', '=C52', '=C52', '=D52', '=D52', '=E52', '=E52'],
+    ['=D42', '=E42', '=E42', '=F42', '^F42', '=G42', '=G42', '=A42', '=A42', '=B42', '=B42', '=C52'],
+    ['=B32', '=C42', '^C42', '=D42', '^D42', '=E42', '=E42', '=F42', '^F42', '=G42', '^G42', '=A42']
+   ]
   if (options.addProps['diatonic'][0]) {
-    keyObject = cloneDeep(_getKeyObject(options.modelKey));
-    keyObject.accidentals = [
-      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
-      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
-      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-    ]; 
-  } else {
-    keyObject = _getKeyObject(options.modelKey);
+    voices[1][4] = '=F42';
+    voices[2] = voices[2].map(symbol => symbol.replace('^', '='));
   }
-
-  return ModelUtilities.getVoicesWithLengthModifications(
-    options.transposeValues, 
-    options.voiceArrangement, 
-    voices, 
-    keyObject, 
-    voicesLength, 
-    measure, 
-    options.addProps
-  );
+  const partLengthValue = options.addProps['partLengthValues'][0];
+  if (partLengthValue !== 6) {
+    voices = voices.map(arr => arr.slice(0, partLengthValue * 2));
+  }
+  const partToBeginValues = options.addProps['partToBeginValues'][0]
+  if (partToBeginValues !== 1) {
+    voices = voices.map(arr => arr.slice((partToBeginValues - 1) * 2));
+  }
+  return ModelUtilities.getVoices(options, voices);
 };
 
 const _adjustMutetVoices = (voices, hideUpperSystem, hideLowerSystem) => {

11aed4ae86752401ed077a1714de34673b9cbb5b
Ulrich Kaiser <ulrich.kaiser@hmtm.de>
2024-11-30 13:35:55 +0100

    Delete test for redactContent method

--- src/progression-models-info.spec.js
+++ src/progression-models-info.spec.js
@@ -9,34 +9,34 @@ describe('example-info', () => {
     sut = new ProgressionModelsInfo(new GithubFlavoredMarkdown());
   });
 
-  describe('redactContent', () => {
-    it('redacts room-media resources from different rooms', () => {
-      const result = sut.redactContent({
-        text: '![Some image](cdn://room-media/63cHjt3BAhGnNxzJGrTsN1/some-image.png)'
-      }, 'rebhjf4MLq7yjeoCnYfn7E');
-      expect(result).toStrictEqual({
-        text: '![Some image]()'
-      });
-    });
+  // describe('redactContent', () => {
+  //   it('redacts room-media resources from different rooms', () => {
+  //     const result = sut.redactContent({
+  //       text: '![Some image](cdn://room-media/63cHjt3BAhGnNxzJGrTsN1/some-image.png)'
+  //     }, 'rebhjf4MLq7yjeoCnYfn7E');
+  //     expect(result).toStrictEqual({
+  //       text: '![Some image]()'
+  //     });
+  //   });
 
-    it('leaves room-media resources from the same room intact', () => {
-      const result = sut.redactContent({
-        text: '![Some image](cdn://room-media/63cHjt3BAhGnNxzJGrTsN1/some-image.png)'
-      }, '63cHjt3BAhGnNxzJGrTsN1');
-      expect(result).toStrictEqual({
-        text: '![Some image](cdn://room-media/63cHjt3BAhGnNxzJGrTsN1/some-image.png)'
-      });
-    });
+  //   it('leaves room-media resources from the same room intact', () => {
+  //     const result = sut.redactContent({
+  //       text: '![Some image](cdn://room-media/63cHjt3BAhGnNxzJGrTsN1/some-image.png)'
+  //     }, '63cHjt3BAhGnNxzJGrTsN1');
+  //     expect(result).toStrictEqual({
+  //       text: '![Some image](cdn://room-media/63cHjt3BAhGnNxzJGrTsN1/some-image.png)'
+  //     });
+  //   });
 
-    it('leaves non room-media resources intact', () => {
-      const result = sut.redactContent({
-        text: '![Some image](cdn://media-library/JgTaqob5vqosBiHsZZoh1/some-image.png)'
-      }, 'rebhjf4MLq7yjeoCnYfn7E');
-      expect(result).toStrictEqual({
-        text: '![Some image](cdn://media-library/JgTaqob5vqosBiHsZZoh1/some-image.png)'
-      });
-    });
-  });
+  //   it('leaves non room-media resources intact', () => {
+  //     const result = sut.redactContent({
+  //       text: '![Some image](cdn://media-library/JgTaqob5vqosBiHsZZoh1/some-image.png)'
+  //     }, 'rebhjf4MLq7yjeoCnYfn7E');
+  //     expect(result).toStrictEqual({
+  //       text: '![Some image](cdn://media-library/JgTaqob5vqosBiHsZZoh1/some-image.png)'
+  //     });
+  //   });
+  // });
 
   describe('getCdnResources', () => {
     it('returns media-library and room-media CDN resources from the text', () => {
